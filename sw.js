86f75f57b12e19179aa76655daa1750dd7ee8ea3
katansapdevelop <katan.sap.development@gmail.com>
2019-01-29 23:35:32 +1000

    Added Changes for Matt's talk

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a9d6bc4ab0e169f8e581.js"
+    "url": "webpack-runtime-5537b956a16c6153f619.js"
   },
   {
     "url": "app-95a5b68d28af0cf8eabf.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "74dd999f7f8a6f2053df807a0bcbff84"
+    "revision": "88d598e4b9b990046950f68f573df48e"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
