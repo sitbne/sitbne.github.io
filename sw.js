7339366898755e277fc8b51e2b59e8824e93f358
katansapdevelop <katan.sap.development@gmail.com>
2019-06-04 23:33:27 +1000

    June 2019 Event

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0382382c6d473216ef90.js"
+    "url": "webpack-runtime-d755444421e22eb140f5.js"
   },
   {
     "url": "app-f694c4c975b037fdb080.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c6de9bb0289a3697f2d3c8a4876e1e5c"
+    "revision": "69b73f3446d4c8cae7397b12ce10049d"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
