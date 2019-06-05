fdbaff34719d1434b35085058d0c2a3ce4c212fe
katansapdevelop <katan.sap.development@gmail.com>
2019-06-05 23:02:46 +1000

    Jons Name Update

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8f969e065f28a02fdfd3.js"
+    "url": "webpack-runtime-a222bc4c9012e72801d9.js"
   },
   {
     "url": "app-f694c4c975b037fdb080.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ce02c10f4911c072e44b1607b6e9ff50"
+    "revision": "794920515f286796e077cd6fe1320288"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
